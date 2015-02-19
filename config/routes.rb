7946c1cad81025bc5948255f6567b0a09ae26569
Michael Price <mike@mightystrongmedia.com>
2015-02-19 10:58:22 -0700

    Add a dynamic product detail page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   resources :charges
 
   # Routes for showing products
-  resources :products, only: [:index]
-  
+  resources :products, only: [:index, :show]
+
   # Routes for showing purchases
   resources :purchases, only: [:show]
 end
